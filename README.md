82ae0bb69552fb895a54a0ad93ba519a5ca15f76
Your Name <you@example.com>
2019-12-03 10:31:12 +0100

    starting changing things

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ Qualsiasi cosa tu voglia creare, puoi usare questo template!
 Il tutorial che abbiamo usato anche in classe lo trovate [qui](https://wbigger.github.io/book-cs-3y/).
 
 Buon divertimento :)
+
+porgetto gestionale
---
+++ gestionale.py
@@ -0,0 +1,13 @@
+
+
+
+class Impegni:
+    def __init__(self, giorno):
+
+        self.ora = []
+
+        for idxElement in range(0,24):
+            self.ora[idxElement] = False
+
+
+appuntamenti = [Impegni, "mercoledi", "giovedi", "venerdi", "sabato"]
